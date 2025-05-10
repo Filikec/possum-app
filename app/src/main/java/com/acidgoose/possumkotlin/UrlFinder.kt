686639f0c6f79bb8645dec3d0b21871c0e181c93
filikec <blabladix@gmail.com>
2025-05-11 00:39:47 +0200

    Updated possum fetching

--- app/src/main/java/com/acidgoose/possumkotlin/UrlFinder.kt
+++ app/src/main/java/com/acidgoose/possumkotlin/UrlFinder.kt
@@ -25,52 +25,13 @@ interface UrlFinder {
 }
 
 /**
- * Finds link on Twitter
- */
-class TwitterUrl : UrlFinder {
-
-    override suspend fun getImageUrl(context : Context): String {
-        val jsonRecentTweets =
-            makeJsonRequest(context, context.getString(R.string.twitter_account_link))
-        val tweetId = jsonRecentTweets.getJSONArray("data").getJSONObject(0).get("id")
-        val jsonLastTweet = makeJsonRequest(context, TWEET_LOOKUP_1 + tweetId + TWEET_LOOKUP_2)
-
-        return jsonLastTweet.getJSONObject("includes")
-            .getJSONArray("media")
-            .getJSONObject(0)
-            .getString("url")
-    }
-
-    /**
-     * Make an http request that returns a json
-     * uses authentication for twitter api
-     */
-    private suspend fun makeJsonRequest(context : Context, url : String) : JSONObject {
-        val client = HttpClient(Android)
-
-        val httpResponse = client.get(url) {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer " + context.getString(R.string.api_token))
-                append(HttpHeaders.UserAgent, "ktor client")
-            }
-        }
-
-        if (httpResponse.status.value in 200..299) {
-            val stringBody: String = httpResponse.body()
-            return JSONObject(stringBody)
-        }
-        return JSONObject()
-    }
-}
-
-/**
  * Finds link on Mastodon
  */
 class MastodonUrl : UrlFinder{
 
     override suspend fun getImageUrl(context: Context): String {
         val client = HttpClient(Android)
-        val httpResponse = client.get("https://botsin.space/api/v1/accounts/109536299782193051/statuses?limit=1")
+        val httpResponse = client.get("https://eu-isoe-west-1.com/api/v1/accounts/113572901986925692/statuses?limit=1")
 
         if (httpResponse.status.value in 200..299) {
             val stringBody: String = httpResponse.body()
